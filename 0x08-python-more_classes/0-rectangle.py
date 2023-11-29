30fca8d55fc62acf124fd6e826b56fa820d61db5
Namisi-Aaron <aaronnamisi@gmail.com>
2023-11-28 22:12:09 -0800

    simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This is a module-level docstring.
+The module creates an empty class Rectangle
+"""
 class Rectangle:
     """This is the docstring for my
     class rectangle"""
