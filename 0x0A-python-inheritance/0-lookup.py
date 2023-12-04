a2608ad742a3268d27a374ee01c7f22dbec93bd3
Namisi-Aaron <aaronnamisi@gmail.com>
2023-12-04 01:21:48 -0800

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""This module defines a function lookup
+
+lookup accepts an object and returns all
+available attributes and methods of it"""
 def lookup(obj):
     return dir(obj)
