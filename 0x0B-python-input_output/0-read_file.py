2a8e490b8ac90702295c7de1bee8a0b66edb90e2
Namisi-Aaron <aaronnamisi@gmail.com>
2023-12-05 16:19:57 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """read_file module"""
 def read_file(filename=""):
+    """reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as myFile:
         print(myFile.read())
