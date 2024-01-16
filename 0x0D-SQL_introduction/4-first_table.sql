e7da4235e5299255645fce2b8b319cf090ff97c2
Namisi-Aaron <aaronnamisi@gmail.com>
2024-01-16 05:36:13 -0800

    first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- creates a table called first_table in the current database in the MySQL server
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
+CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
 	);
