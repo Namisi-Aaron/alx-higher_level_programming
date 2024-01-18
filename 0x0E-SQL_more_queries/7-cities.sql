1cb779016fedcb0b339c676f2d5db89e138f328c
Namisi-Aaron <aaronnamisi@gmail.com>
2024-01-18 04:18:51 -0800

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-	id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT FOREIGN KEY REFERENCES states(id) NOT NULL,
 	name VARCHAR(256) NOT NULL,
 );
