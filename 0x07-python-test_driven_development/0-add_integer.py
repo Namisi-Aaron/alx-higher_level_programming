025df55bd761987fc5740b95a9ce9582becedd1f
Namisi-Aaron <aaronnamisi@gmail.com>
2024-02-05 05:34:56 -0800

    integer addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-This module contains a function that adds two integers
-"""
+"""This module contains a function that adds two integers."""
+
 def add_integer(a, b=98):
-    """ Adds two integer """
+    """Adds two integer."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
