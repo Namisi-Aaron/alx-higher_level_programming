bcb004615b61463abe75ea86c57cb8f4cea0199d
Namisi-Aaron <aaronnamisi@gmail.com>
2024-02-06 03:36:22 -0800

    my first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
+"""My first square module."""
 class Square:
-    """This is an empty class Square.
-
-    The class Square contains an empty
-    declaration
-
-    Attributes:
-        None defined yet
-
-    Args:
-        No arguments have been defined yet
-
-    Raises:
-        No raises are defined for this class
-
-    Methods:
-        No methods are defined for this class yet
-
-    """
+    """This is an empty class Square."""
     pass
