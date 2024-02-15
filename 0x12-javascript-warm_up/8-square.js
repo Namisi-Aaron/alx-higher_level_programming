5be659fb21688cb9a4dc3b281002967a8a03754e
Namisi-Aaron <aaronnamisi@gmail.com>
2024-02-15 03:52:26 -0800

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,13 +5,14 @@ const character = 'X';
 const size = parseInt(process.argv[2]);
 if ((process.argv[2] === undefined) || (isNaN(size))) {
   console.log('Missing size');
-}
-for (i = 0; i < size; i++) {
-  for (j = 0; j < size; j++) {
-    string += character;
+} else {
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      string += character;
+    }
+    if (i !== size - 1) {
+      string += '\n';
+    }
   }
-  if (i !== size - 1) {
-    string += '\n';
-  }
+  console.log(string);
 }
-console.log(string);
