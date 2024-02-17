861e6e28ab5c1c9cb925b9eb8e0bd45a0104d963
Namisi-Aaron <aaronnamisi@gmail.com>
2024-02-17 05:13:37 -0800

    select * states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 
 """This module lists all states from database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
