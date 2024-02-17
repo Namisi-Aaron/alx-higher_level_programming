7855b2aace930dc8614424c798362ba7c25373fa
Namisi-Aaron <aaronnamisi@gmail.com>
2024-02-17 03:04:30 -0800

    get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+def dbConnect(username, password, database):
+    try:
+        connection = MySQLdb.connect(
+                user=username,
+                passwd=password,
+                db=database,
+                host='localhost',
+                port=3306
+                )
+        print("Connected to the database successfully!")
+        return connection
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = dbConnect(username, password, database)
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    connection.close()
