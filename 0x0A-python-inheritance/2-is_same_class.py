5bf97a9721039cca598132c4af7d0c77af108d77
Namisi-Aaron <109237326+Namisi-Aaron@users.noreply.github.com>
2024-03-19 08:49:42 +0300

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module contains the function is_same_class"""
-
-def is_same_class(obj, a_class):
-    """is_same_class returns true if obj is exactly
-    an instance of a_class"""
-
-    return type(obj) is a_class
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""This is the module for BaseGeometry"""
-class BaseGeometry:
-    """This is an empty class"""
-
-    pass
