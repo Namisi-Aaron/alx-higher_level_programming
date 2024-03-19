cab00d55b9f4a68f034d5128108bf0b2badeb4a8
Namisi-Aaron <109237326+Namisi-Aaron@users.noreply.github.com>
2024-03-19 08:50:05 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""from_json_string module"""
-import json
-def from_json_string(my_str):
-    """from_json_string returns an object
-    represented by a json string"""
-
-    return json.loads(my_str)
