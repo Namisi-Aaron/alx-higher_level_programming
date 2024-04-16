1a8af9f7e23b794f22d975d20f4dcfa435f1f82f
Namisi-Aaron <aaronnamisi@gmail.com>
2024-04-16 05:58:55 -0700

    To json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains the function to json string"""
+
+
+import json
+def to_json_string(my_obj):
+    """Returns the json represetation of an object"""
+    return json.dumps(my_obj)
