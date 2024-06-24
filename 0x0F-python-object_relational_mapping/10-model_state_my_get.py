776f46bda9733f0119ea51ab0789f9fa73ffface
Namisi-Aaron <aaronnamisi@gmail.com>
2024-06-24 06:09:24 -0700

    Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script prints the first State object
+    from the database hbtn_0e_6_usa"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+    name = argv[4]
+    connection = 'mysql://{}:{}@localhost:3306/{}'.format(username,
+                                                          password,
+                                                          dbname)
+    engine = create_engine(connection)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State) \
+                   .filter(State.name.like(name)) \
+                   .order_by(State.id) \
+                   .one()
+    if state:
+        print('{}'.format(state.id))
+    else:
+        print('Not found')
+    session.close()
