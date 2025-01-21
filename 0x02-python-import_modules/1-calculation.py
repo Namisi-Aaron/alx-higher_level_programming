c878d4e58ad836e301649761541c1853b3d64657
Namisi-Aaron <aaronnamisi@gmail.com>
2025-01-21 11:08:07 +0300

    imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,5 +8,5 @@ print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
 print("{} / {} = {}".format(a, b, div(a, b)))
 
-if __name__ == "__main__":
+if __name__ != "__main__":
     main()
